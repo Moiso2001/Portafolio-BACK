6d75522a2aaccbca5f88d7378a7d1440c769b5a3
MoisoDavid <wollxi@gmail.com>
2023-02-28 13:48:35 -0500

    Adding a home property to response

--- src/controllers/response.js
+++ src/controllers/response.js
@@ -1,4 +1,14 @@
 module.exports={
+    home: [{msg: "Please type an available route",
+    "0": '/work',
+    "1": '/links',
+    "2": "/experience",
+    "3": "/about/me",
+    "4": "/about/love",
+    "5": "/about/abilities",
+    "6": "/about/aspirations"
+    }],
+    
     aboutabilities: [
         {
             side: 'main',
